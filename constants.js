941ffe0cddf634d8a19af5db1d25540c193364c0
Sahil-0105 <166692650+Sahil-0105@users.noreply.github.com>
2025-08-24 22:01:29 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 export const URL= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
-export const API_KEY = "AIzaSyCMBaX0o70vJ5SRjbyC5PWblb3YL42azTM"; // keep this in .env for security
+
